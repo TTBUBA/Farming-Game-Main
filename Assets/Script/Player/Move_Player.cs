2ad08a48b240231e2a46269c25a40d6a255d16fd
BUBA <122395887+TTBUBA@users.noreply.github.com>
2024-10-14 21:43:08 +0200

    farming game

--- Assets/Script/Player/Move_Player.cs
+++ Assets/Script/Player/Move_Player.cs
@@ -3,46 +3,36 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
-
 public class Move_Player : MonoBehaviour, IData
 {
-    private float horizontal;
-    private float vertical;
+    // Componenti
+    private Rigidbody2D rb;
+    private Animator animator;
 
+    // Movimento
+    private Vector2 movementInput;
     public float lastHorizontal;
     public float lastVertical;
-
-
-    private Rigidbody2D rb;
     public float speed = 7f;
-    public Animator animator;
-
-    private bool isMovingRight;
-    private bool isMovingLeft;
-    private bool isMovingUp;
-
-    public Tilemap Terrainmap; // La Tilemap su cui muoversi
-    public GameObject PointSpawn; // L'oggetto da muovere
-    public bool ActivePointPlant = false;
-    // Variabili che servono per memorizzare le posizioni delle celle
-    private Vector3Int CellPosition; // Posizione della cella corrente
-    private Vector3Int NextCellPosition; // Posizione della prossima cella da raggiungere
 
     // Input System
     public InputActionReference MovePlayer_KeyBoard;
     public InputActionReference MovePlayer_Controller;
 
-    public PlayerInput playerInput;
+    // Stato di movimento tramite pulsanti mobili
+    private Vector2 mobileMovementDirection = Vector2.zero;
+    private bool isMobileMovementPressed = false;
 
+    // Oggetti e Tilemap
+    public Tilemap Terrainmap;
+    public GameObject PointSpawn;
     public GameObject Axe;
+    public bool ActivePointPlant = false;
 
+    // Posizioni delle celle
+    private Vector3Int CellPosition;
+    private Vector3Int NextCellPosition;
 
-    public Vector2 Movement;
-    public Vector2 keyboardMovement;
-    public Vector2 controllerMovement;
-
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -51,230 +41,255 @@ public class Move_Player : MonoBehaviour, IData
 
     public void LoadData(GameData data)
     {
-        this.transform.position = data.PositionPlayer;
+        transform.position = data.PositionPlayer;
     }
 
     public void SaveData(GameData data)
     {
-        data.PositionPlayer = this.transform.position;
+        data.PositionPlayer = transform.position;
     }
 
-    //Input Controller//
     private void OnEnable()
     {
         MovePlayer_KeyBoard.action.Enable();
         MovePlayer_Controller.action.Enable();
     }
+
     private void OnDisable()
     {
         MovePlayer_KeyBoard.action.Disable();
         MovePlayer_Controller.action.Disable();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        HandleInput();
+        HandleMovement();
+        HandleAnimation();
+        HandleAxePosition();
+    }
 
+    private void HandleInput()
+    {
+        // Legge gli input da tastiera e controller e li combina
+        Vector2 keyboardMovement = MovePlayer_KeyBoard.action.ReadValue<Vector2>();
+        Vector2 controllerMovement = MovePlayer_Controller.action.ReadValue<Vector2>();
 
-        keyboardMovement = MovePlayer_KeyBoard.action.ReadValue<Vector2>();
-        controllerMovement = MovePlayer_Controller.action.ReadValue<Vector2>();
+        movementInput = (keyboardMovement + controllerMovement).normalized;
 
-
-        Movement = (keyboardMovement + controllerMovement).normalized;
-
-        horizontal = Movement.x;
-        vertical = Movement.y;
-
-       // Debug.Log(horizontal);
-        // Il player rimane nella posizione in qui era prima
-        if (horizontal != 0 || vertical != 0)
+        // Se c'è movimento mobile attivo, sovrascrive gli input di tastiera/controller
+        if (isMobileMovementPressed)
         {
-            lastHorizontal = horizontal;
-            lastVertical = vertical;
+            movementInput = mobileMovementDirection;
         }
 
-        animator.SetBool("IdleDown", false);
-        animator.SetBool("IdleUp", false);
-        animator.SetBool("IdleLeft", false);
-        animator.SetBool("IdleRight", false);
+        // Salva l'ultimo movimento se il giocatore si sta muovendo
+        if (movementInput.magnitude > 0)
+        {
+            lastHorizontal = movementInput.x;
+            lastVertical = movementInput.y;
+        }
+    }
 
+    private void HandleMovement()
+    {
+        // Muovi il personaggio
+        rb.velocity = movementInput * speed;
+
+        // Muovi PointSpawn se necessario
+        if (ActivePointPlant)
+        {
+            MovePointSpawn();
+        }
+    }
+
+    private void HandleAnimation()
+    {
+        // Imposta i parametri di movimento per l'animatore
         animator.SetFloat("Horizontal", lastHorizontal);
         animator.SetFloat("Vertical", lastVertical);
 
-        if(horizontal == 0 && vertical == 0)
+        // Se il personaggio è fermo, mantieni l'ultima direzione per l'animazione di Idle
+        if (movementInput.magnitude == 0)
         {
             switch (DetermineDirection(lastHorizontal, lastVertical))
             {
                 case Direction.Down:
                     animator.SetBool("IdleDown", true);
                     break;
-
                 case Direction.Up:
                     animator.SetBool("IdleUp", true);
                     break;
-
                 case Direction.Left:
                     animator.SetBool("IdleLeft", true);
                     break;
-
                 case Direction.Right:
                     animator.SetBool("IdleRight", true);
                     break;
-
             }
-                
-
-
         }
-
-        Vector2 direction = new Vector2(horizontal, vertical);
-
-        if (direction.magnitude > 0)
+        else
         {
-            direction.Normalize();
+            // Resetta Idle se il giocatore si sta muovendo
+            ResetIdleAnimation();
         }
-
-        rb.velocity = direction * speed;
-
-
-        // Controlla la direzione in orizzontale del movimento dell ascia & PointSpawn
-        switch (lastHorizontal)
-        {
-            case > 0:
-                //Movimento ascia
-                Axe.transform.localPosition = new Vector2(0.3f, -0.08f);
-                Axe.transform.localScale = new Vector2(0.5f, 0.5f);
-
-                //Movimento PointSpawn 
-                PointSpawn.transform.localPosition = new Vector2(1, 0);
-                break;
-            case < 0:
-                //Movimento ascia
-                Axe.transform.localPosition = new Vector2(-0.3f, -0.08f);
-                Axe.transform.localScale = new Vector2(-0.5f, 0.5f);
-
-                //Movimento PointSpawn 
-                PointSpawn.transform.localPosition = new Vector2(-1, 0);
-                break;
-        }
-
-        // Controlla la direzione in verticale del movimento del PointSpawn degli ortaggi
-        switch (lastVertical)
-        {
-            case > 0:
-                PointSpawn.transform.localPosition = new Vector2(0, 1);
-                break;
-            case < 0:
-                PointSpawn.transform.localPosition = new Vector2(0, -1);
-                break;
-        }
-
-        if (ActivePointPlant == true)
-        {
-            MovimentoPointSpawn();
-        }
-
     }
+
+    private void HandleAxePosition()
+    {
+        // Imposta la posizione e la scala dell'ascia in base al movimento
+        if (lastHorizontal > 0)
+        {
+            Axe.transform.localPosition = new Vector2(0.3f, -0.08f);
+            Axe.transform.localScale = new Vector2(0.5f, 0.5f);
+            PointSpawn.transform.localPosition = new Vector2(1, 0);
+        }
+        else if (lastHorizontal < 0)
+        {
+            Axe.transform.localPosition = new Vector2(-0.3f, -0.08f);
+            Axe.transform.localScale = new Vector2(-0.5f, 0.5f);
+            PointSpawn.transform.localPosition = new Vector2(-1, 0);
+        }
+
+        if (lastVertical > 0)
+        {
+            PointSpawn.transform.localPosition = new Vector2(0, 1);
+        }
+        else if (lastVertical < 0)
+        {
+            PointSpawn.transform.localPosition = new Vector2(0, -1);
+        }
+    }
+
+    // Muovi PointSpawn nella prossima cella 
+    private void MovePointSpawn()
+    {
+        // Calcola la cella corrente
+        CellPosition = Terrainmap.WorldToCell(transform.position);
+
+        // Muovi PointSpawn nella prossima cella
+        if (lastHorizontal > 0)
+        {
+            NextCellPosition = new Vector3Int(CellPosition.x + 1, CellPosition.y, 0);
+        }
+        else if (lastHorizontal < 0)
+        {
+            NextCellPosition = new Vector3Int(CellPosition.x - 1, CellPosition.y, 0);
+        }
+
+        if (lastVertical > 0)
+        {
+            NextCellPosition = new Vector3Int(CellPosition.x, CellPosition.y + 1, 0);
+        }
+        else if (lastVertical < 0)
+        {
+            NextCellPosition = new Vector3Int(CellPosition.x, CellPosition.y - 1, 0);
+        }
+
+        PointSpawn.transform.position = Terrainmap.GetCellCenterWorld(NextCellPosition);
+    }
+
+    private void ResetIdleAnimation()
+    {
+        animator.SetBool("IdleDown", false);
+        animator.SetBool("IdleUp", false);
+        animator.SetBool("IdleLeft", false);
+        animator.SetBool("IdleRight", false);
+    }
+
+    // Funzioni per il movimento mobile (pulsanti su/giù/sinistra/destra)
+    public void MovementMobile_Down()
+    {
+        SetMobileMovement(0f, -1f);
+    }
+
+    public void MovementMobile_Down_Up()
+    {
+        StopMobileMovement();
+    }
+
+    public void MovementMobile_Up()
+    {
+        SetMobileMovement(0f, 1f);
+    }
+
+    public void MovementMobile_Up_Up()
+    {
+        StopMobileMovement();
+    }
+
+    public void MovementMobile_Left()
+    {
+        SetMobileMovement(-1f, 0f);
+    }
+
+    public void MovementMobile_Left_Up()
+    {
+        StopMobileMovement();
+    }
+
+    public void MovementMobile_Right()
+    {
+        SetMobileMovement(1f, 0f);
+    }
+
+    public void MovementMobile_Right_Up()
+    {
+        StopMobileMovement();
+    }
+
+    private void SetMobileMovement(float x, float y)
+    {
+        mobileMovementDirection = new Vector2(x, y).normalized;
+        isMobileMovementPressed = true;
+    }
+
+    private void StopMobileMovement()
+    {
+        isMobileMovementPressed = false;
+    }
+
+    private Direction DetermineDirection(float horizontal, float vertical)
+    {
+        if (vertical < 0)
+        {
+            return Direction.Down;
+        }
+        else if (vertical > 0)
+        {
+            return Direction.Up;
+        }
+        else if (horizontal < 0)
+        {
+            return Direction.Left;
+        }
+        else
+        {
+            return Direction.Right;
+        }
+    }
+
+    private enum Direction
+    {
+        Down,
+        Up,
+        Left,
+        Right
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Terreno"))
         {
-            if (gameObject.CompareTag("BoxPlayer") || gameObject.CompareTag("Player"))
-            {
-                ActivePointPlant = true;
-            }
+            ActivePointPlant = true;
         }
     }
 
     public void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Terreno") || gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Terreno"))
         {
-            if (gameObject.CompareTag("BoxPlayer") || gameObject.CompareTag("Player"))
-            {
-                ActivePointPlant = true;
-            }
+            ActivePointPlant = false;
         }
     }
-    // Funzione che serve per far muovere l'oggetto PointSpawn sulla Tilemap
-    public void MovimentoPointSpawn()
-    {
-        // Ottieni la posizione della cella corrente in cui si trova il giocatore
-        CellPosition = Terrainmap.WorldToCell(transform.position);
-
-        // Ottieni il centro della cella corrente
-        Vector3 CellCenter = Terrainmap.GetCellCenterWorld(CellPosition);
-
-        switch (horizontal)
-        {
-            case > 0:
-                // Calcola la posizione della prossima cella aumentando di uno l'indice x rispetto alla cella corrente
-                NextCellPosition = new Vector3Int(CellPosition.x + 1, CellPosition.y);
-                ;
-                // Sposta l'oggetto PointSpawn al centro della prossima cella calcolata da NextCellPosition
-                PointSpawn.transform.position = Terrainmap.GetCellCenterWorld(NextCellPosition);
-                break;
-
-            case < 0:
-                // Calcola la posizione della prossima cella aumentando di uno l'indice x rispetto alla cella corrente
-                NextCellPosition = new Vector3Int(CellPosition.x - 1, CellPosition.y);
-
-                // Sposta l'oggetto PointSpawn al centro della prossima cella calcolata da NextCellPosition
-                PointSpawn.transform.position = Terrainmap.GetCellCenterWorld(NextCellPosition);
-                break;
-        }
-
-        switch (vertical)
-        {
-            case > 0:
-                // Calcola la posizione della prossima cella aumentando di uno l'indice x rispetto alla cella corrente
-                NextCellPosition = new Vector3Int(CellPosition.x, CellPosition.y + 1);
-
-                // Sposta l'oggetto PointSpawn al centro della prossima cella calcolata da NextCellPosition
-                PointSpawn.transform.position = Terrainmap.GetCellCenterWorld(NextCellPosition);
-                break;
-
-            case < 0:
-                // Calcola la posizione della prossima cella aumentando di uno l'indice x rispetto alla cella corrente
-                NextCellPosition = new Vector3Int(CellPosition.x, CellPosition.y - 1);
-
-                // Sposta l'oggetto PointSpawn al centro della prossima cella calcolata da NextCellPosition
-                PointSpawn.transform.position = Terrainmap.GetCellCenterWorld(NextCellPosition);
-                break;
-        }
-    }
-
-    private enum Direction 
-     { 
-        Down, 
-        Up, 
-        Left, 
-        Right,
-     }
-
-    private Direction DetermineDirection(float Horizontal , float Vertical)
-    {
-        if (lastVertical < 0)
-        {
-            return Direction.Down;
-        }
-        else if (lastVertical > 0)
-        {
-            return Direction.Up;
-        }
-        else if (lastHorizontal < 0)
-        {
-            return Direction.Left;
-        }
-        else if(lastHorizontal > 0)
-        {
-             return Direction.Right;
-        }
-        else
-        {
-            return Direction.Down;
-        }
-    }
-
-  
 }
